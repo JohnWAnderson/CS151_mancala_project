b7d1ae55509d6a13a0a9e3c3bb62b214603bd5ca
dr-p <csdalporto@gmail.com>
2015-11-08 16:41:40 -0800

    Revert "test"
    This reverts commit 494fd858fff8ea10640880f77a8354112101257e.

--- MancProject/src/Mancala.java
+++ MancProject/src/Mancala.java
@@ -5,6 +5,6 @@ public class Mancala
 		MancalaBoard game = new MancalaBoard();
 		System.out.println("First line added testing commit system");
 		System.out.println("Second commit after deleting and importing repo.");
-		// test    
+		// test
 	}
 }

e1fbb2560790594dda7876928124a155c7ee1884
dr-p <csdalporto@gmail.com>
2015-11-18 14:26:24 -0800

    While loop

--- MancProject/src/Mancala.java
+++ MancProject/src/Mancala.java
@@ -5,5 +5,9 @@ public class Mancala
 		MancalaBoard game = new MancalaBoard();
 		System.out.println("First line added testing commit system");
 		System.out.println("Second commit after deleting and importing repo.");
+		int i = 1;
+		while(i != 0) {
+			System.out.println("Best loop ever");
+		}
 	}
 }

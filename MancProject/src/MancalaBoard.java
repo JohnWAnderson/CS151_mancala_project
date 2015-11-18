c2368b462d4782938969f1e4bfe3b6e0107f234f
John <John.Anderson45@yahoo.com>
2015-11-18 14:36:53 -0800

    just testing

--- MancProject/src/MancalaBoard.java
+++ MancProject/src/MancalaBoard.java
@@ -9,32 +9,39 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class MancalaBoard
+public class MancalaBoard implements Strategy
 {	
 	private ArrayList<Pit> pits = new ArrayList<Pit>();
-	public MancalaBoard()
-	{
+	private int type;
+	private Color color;
+	public MancalaBoard(Color color, int type, int numberStones)
+	{  
+		this.color = color;   
+		this.type = type;
 		JFrame frame = new JFrame("mancala");
-		frame.setSize(1000,500);
-		JPanel center = new JPanel(new GridLayout(2,6));
-		center.setPreferredSize(new Dimension(600,400));
+		frame.setSize(1000, 300);	
+		JPanel center = new JPanel();
+		center.setLayout(new GridLayout(2,6));
 		for(int i =0; i < 12; i++)
 		{
-			pits.add(new Pit());
-			center.add(pits.get(i));
+			pits.add(new Pit(100, 100, this.type, this.color));
+			JLabel temp = new JLabel(pits.get(i));
+			center.add(temp);
 		}
-		JPanel bigpit1 = new JPanel();	bigpit1.setPreferredSize(new Dimension(200,400)); bigpit1.add(new BigPit());
-		JPanel bigpit2 = new JPanel();  bigpit2.setPreferredSize(new Dimension(200,400)); bigpit2.add(new BigPit());
-		JPanel top = new JPanel(); top.setPreferredSize(new Dimension(1000, 50));
-		JPanel bot = new JPanel(); bot.setPreferredSize(new Dimension(1000, 50));
+		JLabel big1 = new JLabel(new BigPit(150,200,this.type,this.color));
+		JLabel big2 = new JLabel(new BigPit(150,200,this.type,this.color));
+		JPanel bigpit1 = new JPanel();
+		JPanel bigpit2 = new JPanel(); 
+		bigpit1.add(big1);
+		bigpit2.add(big2);
+		JPanel top = new JPanel();
+		JPanel bot = new JPanel(); 
 		for(int i =1; i < 7; i++)
 		{
-			JLabel temp = new JLabel("A" + i );
-			temp.setPreferredSize(new Dimension(100,50));
-			top.add(temp);
-			JLabel tempB = new JLabel("B" + i );
-			tempB.setPreferredSize(new Dimension(90,50));
-			bot.add(tempB);
+			JLabel tempB = new JLabel( "               " + "B" + (7 - i) + "                ");
+			top.add(tempB); 
+			JLabel tempA = new JLabel("               " + "A" + i + "                ");
+			bot.add(tempA);
 		}
 		
 		frame.add(bigpit1, BorderLayout.EAST);
@@ -45,12 +52,15 @@ public class MancalaBoard
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
-	public void addPits(JPanel center)
-	{
-		for(int i = 0;i < 12; i++)
-		{
-			
-		}
+	@Override
+	public int ShapeFormatter() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public int NumberOfStones() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 	
 }

2ca8df4899ea3a21d1d10694ff6376661a29455b
John <John.Anderson45@yahoo.com>
2015-11-20 14:13:51 -0800

    better start up type

--- MancProject/src/MancalaProject/Mancala.java
+++ MancProject/src/MancalaProject/Mancala.java
@@ -1,12 +1,35 @@
 package MancalaProject;
 
+import javax.swing.JOptionPane;
+
 public class Mancala
 {	
 	public static void main(String[] args)
 	{
-		StartUp menu = new StartUp();
-		
-		System.out.println("First line added testing commit system");
-		System.out.println("Second commit after deleting and importing repo.");
+
+		int selection = JOptionPane.showConfirmDialog(null,
+				"Would you like to play a game", "Mancala",
+				JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE);
+		if (selection == -1 || selection == 2) {
+			JOptionPane.showMessageDialog(null, "GoodBye");
+			System.exit(0);
+		}
+		int option = JOptionPane.showOptionDialog(null,
+				"Pick what game stlye that ", "Mancala Game",
+				JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null,
+				MancalaHold.SETUP, MancalaHold.SETUP[0]);
+		int stones = JOptionPane.showOptionDialog(null,
+				"How many stones would you like to start with?",
+				"Mancala Game", JOptionPane.YES_NO_OPTION,
+				JOptionPane.QUESTION_MESSAGE, null, MancalaHold.SETUP_2, null);
+		if (stones == -1) {
+			JOptionPane.showMessageDialog(null, "GoodBye");
+			System.exit(0);
+		}
+		//if (stones == 0)
+		//	model.setInitStones(3);
+		//else
+		//	model.setInitStones(4);
+		//model.update();
 	}
 }   
---
+++ MancProject/src/MancalaProject/MancalaHold.java
@@ -0,0 +1,7 @@
+package MancalaProject;
+
+public class MancalaHold
+{
+	public final static String[] SETUP = { "Clasic", "Custom" };
+	public final static String[] SETUP_2 = { "3 Stones", "4 Stones" };
+}

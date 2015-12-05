0123a2aca274d95cc232be93b21e8e8c3a063b6c
John <John.Anderson45@yahoo.com>
2015-12-04 22:52:06 -0800

    Gui Fix

--- MancProject/src/MancalaProject/custom.java
+++ MancProject/src/MancalaProject/custom.java
@@ -27,7 +27,8 @@ public class custom implements Borad
 	@Override
 	public void drawbigpits(Graphics2D g2, int x, int y, int stones, int size)  {
 		Rectangle2D.Double pit = new Rectangle2D.Double(0, 0, size/2 -5, size - 5);
-		g2.draw(pit);
+		g2.setColor(Color.BLACK);
+		 g2.fill(pit);
 			for(int i = 0; i < stones; i ++)
 			 {
 				Ellipse2D.Double temp = new Ellipse2D.Double(bigstonesspot()+15, bigstonesspot()+60, 15, 15);
@@ -35,7 +36,7 @@ public class custom implements Borad
 				 g2.fill(temp);
 			 }
 		String temp = String.valueOf(stones);
-		g2.setColor(Color.BLACK);
+		g2.setColor(Color.WHITE);
 		g2.drawString(temp, x+10, y+12);
 	}
 	/**
@@ -44,7 +45,8 @@ public class custom implements Borad
 	@Override
 	public void drawpits(Graphics2D g2, int x, int y, int stones, int size) {
 		Rectangle2D.Double pit = new Rectangle2D.Double(0, 0, size, size);
-		g2.draw(pit);
+		g2.setColor(Color.BLACK);
+		 g2.fill(pit);
 			for(int i = 0; i < stones; i ++)
 			 {
 				Ellipse2D.Double temp = new Ellipse2D.Double(stonespot()+25, stonespot()+30, 15, 15);
@@ -52,7 +54,7 @@ public class custom implements Borad
 				 g2.fill(temp);
 			 }
 		String temp = String.valueOf(stones);
-		g2.setColor(Color.BLACK);
+		g2.setColor(Color.WHITE);
 		g2.drawString(temp, x+5, y);
 	}
 	/**
